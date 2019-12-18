772e6bb87b73b696c06a6ff3c72342a2682813b0
Wai Kit <wkitsin94@hotmail.com>
2019-12-19 01:20:48 +0800

    added item and user type in graphql

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,13 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, String, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :full_name, String, null: false
+    field :email, String, null: false
+
+    def full_name
+      [object.first_name, object.last_name].compact.join(' ')
+    end
+  end
+end
